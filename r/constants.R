2f1e2795d6f13b183f506f0446273ee347b706bb
Don Boyd <donboyd5@gmail.com>
2023-06-06 13:55:37 -0400

    WIP

--- r/constants.R
+++ r/constants.R
@@ -10,6 +10,8 @@ legend_none <- theme(legend.position = "None")
 legend_notitle <- theme(legend.title = element_blank())
 caption_left <- theme(plot.caption = element_text(hjust = 0))
 
+x90 <- theme(axis.text.x = element_text(angle = -90, vjust = 0, hjust=0.5))
+
 # constants ---------------------------------------------------------------
 
 # MCTD includes the counties of:
